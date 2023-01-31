1a16f5b60de8c5fb903d029ff9644de11029f55a
Vitalik <gv_2004@ukr.net>
2023-01-31 21:56:34 +0200

    1

---
+++ lessons/lesson 1/main.js
@@ -0,0 +1,27 @@
+let str = 'Hello';
+console.log('Hello');
+console.log('owu');
+console.log('com');
+console.log('ua');
+
+let num = 1;
+console.log('10');
+console.log('-999');
+console.log('123');
+console.log('3.14');
+console.log('2.7');
+console.log('16');
+
+let boot =true;
+console.log('true');
+console.log('false');
+
+let firstName= 'Гудак';
+let middleName= 'Віталій';
+let lastName= 'Сергійович';
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+let a = '100';
+let b = '"100"';
+let c = true;
+console.log(typeof a, b, c)
