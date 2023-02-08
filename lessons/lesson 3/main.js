0aa694b4f47f448a9c50c3e8ef18728ae80d3281
Vitalik <gv_2004@ukr.net>
2023-02-08 17:25:53 +0200

    7

--- lessons/lesson 3/main.js
+++ lessons/lesson 3/main.js
@@ -5,19 +5,19 @@ for (let i = 0; i < 10; i++) {
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 for (let i = 0; i < 10; i++) {
     document.write(`<div> ${i + 1} - Довільний текст всередині<div/>`)
-        }
+}
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 for (let i = 0; i < 20; i++)
-while (i < 20) {
-    document.write(`<h1> Довільний текст всередині</h1>`)
-    i++;
-}
+    while (i < 20) {
+        document.write(`<h1> Довільний текст всередині</h1>`)
+        i++;
+    }
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
 for (let i = 0; i < 20; i++)
-while (i < 20) {
-    document.write(`<h1>${i + 1} - Довільний текст всередині</h1>`)
-    i++;
-}
+    while (i < 20) {
+        document.write(`<h1>${i + 1} - Довільний текст всередині</h1>`)
+        i++;
+    }
 // - Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
 // Масив:
@@ -37,15 +37,11 @@ while (i < 20) {
 //
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-
+`<ul>`
 for (let items of listOfItems) {
-    document.write(`<ul>
-          <li>
-          ${items} 
-      </li>
-</ul>`)
+    document.write(`<li>${items}</li>`)
 }
-
+`</ul>`
 // -----------------------------------------------
 //
 //     Використовуючи данні з масиву, за допомоги document.write та циклу
@@ -135,4 +131,39 @@ for (let product of products) {
 //     - користувачів зі статусом true
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}];
+`<ul>`
+for (let user of users) {
+    if (user.status === true)
+    document.write(`<li>${user.name} ${user.status}</li>`)
+}
+`</ul>`
+{
+    `<ul>`
+    for (let user of users) {
+        if (user.status === false)
+            document.write(`<li>${user.name} ${user.status}</li>`)
+    }
+    `</ul>`
+}
+{
+    `<ul>`
+    for (let user of users) {
+        if (user.age >30)
+            document.write(`<li>${user.name} ${user.age}</li>`)
+    }
+    `</ul>`
+}
 
